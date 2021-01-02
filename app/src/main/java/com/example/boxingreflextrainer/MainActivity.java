806837a7eb80658bc44633a33c04445b4f2c6914
GiovanniNeve <thomasz01@outlook.it>
2021-01-02 17:39:11 +0100

    Warnings fixed

--- app/src/main/java/com/example/boxingreflextrainer/MainActivity.java
+++ app/src/main/java/com/example/boxingreflextrainer/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.boxingreflextrainer;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,9 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceManager;
-
-import java.util.Timer;
 
 
 public class MainActivity extends AppCompatActivity {
