5fb8cb047aad9299106ebc273da94cf5a00498fa
GiovanniNeve <thomasz01@outlook.it>
2021-01-06 14:39:50 +0100

    Round View fix

--- app/src/main/java/com/example/boxingreflextrainer/TimerHandler.java
+++ app/src/main/java/com/example/boxingreflextrainer/TimerHandler.java
@@ -93,7 +93,7 @@ public class TimerHandler {
                         isRestTime = false;
                         isActive = false;
                         isPreparationTime = true;
-                        roundIterator--;
+                        roundIterator = 0;
                         getPreferences();
                         stopTimer();
 
